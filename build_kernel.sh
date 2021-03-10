c3ceb51093567c735f4379c0a8a7a98da9331db0
abasgames <45899491+abasgames@users.noreply.github.com>
2021-03-10 18:18:56 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,5 +2,6 @@ git clone https://github.com/abasgames/android_kernel_samsung_universal7580
 echo clone kernel done
 cd android_kernel_samsung_universal7580
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+export aarch64-linux-android-4.9/bin/aarch64-linux-android-gcc-
 echo clone gcc done
 sh ./build_kernel.sh
